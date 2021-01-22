1de643369768506550dacbc32b7bcb64c34d6d23
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2021-01-23 00:27:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -51,8 +51,8 @@ wget https://github.com/Mattaclp/NewLEDE/raw/main/aarch64_generic/target.mk
 mv target.mk target/linux/armvirt/64/
 
 # change Xray-core
-#sed -i 's/PKG_VERSION:=1.2.1/PKG_VERSION:=1.2.2/g' package/lean/xray/Makefile
-#sed -i 's/eebea67d9176cc016d21de2d8123b0a6ab795ec82af2c6b8d7467212841bdc56/b9b38bc69e660d3a6d0545efea4d0891ee81a2d55c83a6fa893cbd680a071023/g' package/lean/xray/Makefile
+sed -i 's/PKG_VERSION:=1.2.2/PKG_VERSION:=1.2.3/g' package/lean/xray/Makefile
+sed -i 's/b9b38bc69e660d3a6d0545efea4d0891ee81a2d55c83a6fa893cbd680a071023/1deed281d2b976c0132b57194a09b62a1b978ec1b35d8329894b80f8d47befb7/g' package/lean/xray/Makefile
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
