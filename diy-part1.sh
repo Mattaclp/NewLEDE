94d2ff9f0d4786824effed65f9c62910da3af57e
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2021-01-25 22:56:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -46,9 +46,9 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/AdGuardHome package/lean/AdGuardHome
 
 # set ARM to aarch64_generic
-#rm -rf target/linux/armvirt/64/target.mk
-#wget https://github.com/Mattaclp/NewLEDE/raw/main/aarch64_generic/target.mk
-#mv target.mk target/linux/armvirt/64/
+rm -rf target/linux/armvirt/64/target.mk
+wget https://github.com/Mattaclp/NewLEDE/raw/main/aarch64_generic/target.mk
+mv target.mk target/linux/armvirt/64/
 
 # change Xray-core
 #sed -i 's/PKG_VERSION:=1.2.2/PKG_VERSION:=1.2.3/g' package/lean/xray/Makefile
