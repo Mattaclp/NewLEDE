47dd2ceda50a9d0eaab1e678e29b615fec1b6d89
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2021-10-03 09:09:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,8 @@
 git clone https://github.com/fw876/helloworld.git package/helloworld
 
 # add passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy1
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy1
+git clone https://github.com/chiwaicw/openwrt-passwall.git package/diy1
 
 # Add my helloworld
 #sed -i '$a src-git helloworld https://github.com/Mattaclp/helloworld' feeds.conf.default
