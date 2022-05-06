619cfd7960b9e00f1d8eebe223b08052625babc9
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2022-05-06 10:44:51 +0800

    Update diy-part1-lede-stable.sh

--- diy-part1-lede-stable.sh
+++ diy-part1-lede-stable.sh
@@ -134,7 +134,7 @@ popd
 
 
 # Add clash
-git clone -b master https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
+#git clone -b master https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
 
 # sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' ./target/linux/ipq40xx/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=4.14/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/ipq40xx/Makefile
