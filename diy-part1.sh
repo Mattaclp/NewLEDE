876b73e09f0522a716a0761838f9180cbf702da1
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2022-07-14 18:04:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -43,7 +43,9 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 #sed -i '$a src-git immortalwrtpackages https://github.com/chiwaicw/immortalwrtpackages' feeds.conf.default
 #sed -i '$a src-git immortalwrtluci https://github.com/chiwaicw/immortalwrtluci' feeds.conf.default
 sed -i '$a src-git immortalwrtpackages https://github.com/chiwaicw/immortalwrtpackagesslim' feeds.conf.default
-sed -i '$a src-git immortalwrtluci https://github.com/chiwaicw/immortalwrtlucislim' feeds.conf.default
+#sed -i '$a src-git immortalwrtluci https://github.com/chiwaicw/immortalwrtlucislim' feeds.conf.default
+svn co https://github.com/chiwaicw/immortalwrtlucislim/trunk/applications/luci-app-filebrowser package/luci-app-filebrowser
+svn co https://github.com/chiwaicw/immortalwrtlucislim/trunk/applications/luci-app-gowebdav package/luci-app-gowebdav
 #svn co https://github.com/chiwaicw/immortalwrtpackages/trunk/net/gowebdav package/lean/gowebdav
 #svn co https://github.com/chiwaicw/immortalwrtluci/trunk/applications/luci-app-gowebdav package/lean/luci-app-gowebdav
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
