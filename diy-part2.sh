c9828a82fdb9e7ae3ff5ac1ba8440ebdc0f109b9
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2023-01-09 22:49:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,12 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
+#luci-app-alist
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+svn co https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
+svn co https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
+
 # Add shadowsocks-rust 1.15.0-alpha.4
 #rm -rf package/helloworld/shadowsocks-rust
 #svn co  https://github.com/Mattaclp/NewLEDE/trunk/shadowsocks-rust package/helloworld/shadowsocks-rust
