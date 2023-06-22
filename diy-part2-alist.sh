89a3748618ebca45d2b8e899229c88c5f3b97dc1
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2023-06-22 09:21:13 +0800

    Update diy-part2-alist.sh

--- diy-part2-alist.sh
+++ diy-part2-alist.sh
@@ -13,8 +13,8 @@ sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_gener
 
 #luci-app-alist
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
-#svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+#svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 
 # Add shadowsocks-rust 1.15.0-alpha.4
 #rm -rf package/helloworld/shadowsocks-rust
