d860cdfc0684ac9ac5084ee68266469935e83e2a
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2024-02-15 17:31:12 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -4,10 +4,6 @@
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
 
-# curl/8.5.0 - fix passwall `time_pretransfer` check
-rm -rf feeds/packages/net/curl
-git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
-
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
