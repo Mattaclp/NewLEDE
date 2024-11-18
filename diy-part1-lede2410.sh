16b903a40744081ded6825bf5324ed2cd66d4f74
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2024-11-18 12:05:43 +0800

    Create diy-part1-lede2410.sh

---
+++ diy-part1-lede2410.sh
@@ -0,0 +1,7 @@
+# add vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+mv lua-maxminddb package/lean/
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/Mattaclp/luci-app-vssr.git
+mv luci-app-vssr package/lean/
+
