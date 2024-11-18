7d952522d6d84ebb968ccebcdfbbe50063a4f488
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2024-11-18 09:41:43 +0800

    Create diy-part2-lede2410.sh

---
+++ diy-part2-lede2410.sh
@@ -0,0 +1,12 @@
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+
+# 移除重复软件包
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon-mod
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
