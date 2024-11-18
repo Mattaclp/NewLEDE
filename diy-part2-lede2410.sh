eb8a195d917a2f840cc8d28166a61a70602f0ba1
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2024-11-19 07:33:36 +0800

    Update diy-part2-lede2410.sh

--- diy-part2-lede2410.sh
+++ diy-part2-lede2410.sh
@@ -1,6 +1,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 
+sed -i "s/hostname='LEDE'/hostname='OpenWrt'/g" ./package/base-files/files/bin/config_generate
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 #rm -rf feeds/luci/themes/luci-theme-argon-mod
