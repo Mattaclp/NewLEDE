3cd3997b7e5601834df56ffa24264a0da2f8b892
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-05-07 11:06:25 +0800

    Update diy-part2-lede2410.sh

--- diy-part2-lede2410.sh
+++ diy-part2-lede2410.sh
@@ -6,9 +6,9 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 #rm -rf feeds/luci/applications/luci-app-argon-config
 #git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-rm -rf feeds/packages/net/smartdns feeds/luci/applications/luci-app-smartdns
-git clone --depth=1 -b master https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
-git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/packages/net/smartdns feeds/luci/applications/luci-app-smartdns
+#git clone --depth=1 -b master https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
+#git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/luci2/bin/config_generate
