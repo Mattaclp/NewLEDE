a16478e62a1b823a59f0add66c225bbfa4f5ded2
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-06-01 19:26:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -92,7 +92,8 @@ mv luci-app-adguardhome package/lean/
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 mv lua-maxminddb package/lean/
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/Mattaclp/luci-app-vssr.git
+git clone https://github.com/v03413/luci-app-vssr.git
+#git clone https://github.com/Mattaclp/luci-app-vssr.git
 mv luci-app-vssr package/lean/
 
 # add luci-app-casaJJJos
