6b1dd82f5d6fe9b3edd69fc25395eb3ab54af9d1
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2025-07-27 23:45:36 +0800

    Update diy-part1-lede2410.sh

--- diy-part1-lede2410.sh
+++ diy-part1-lede2410.sh
@@ -1,12 +1,12 @@
-sed -i '$a src-git mihomo https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
+#sed -i '$a src-git mihomo https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
 
 # add vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-mv lua-maxminddb package/lean/
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
+#mv lua-maxminddb package/lean/
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/Mattaclp/luci-app-vssr.git
-mv luci-app-vssr package/lean/
+#git clone https://github.com/Mattaclp/luci-app-vssr.git
+#mv luci-app-vssr package/lean/
 # add luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
-mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
-rm -rf package-temp
+#git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
+#mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
+#rm -rf package-temp
